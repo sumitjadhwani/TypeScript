6df695367261112222fbe49fb540f9982129270d
Sumit Jadhwani <42548520+sumitjadhwani@users.noreply.github.com>
2022-06-11 07:46:36 +0000

    Functions with optional params and type

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,17 @@
 "use strict";
 console.log('hello ');
+let age = 20;
+let bigNo = 1000222;
+console.log(bigNo);
+let str = 'hjgskj';
+let tuple = [1, 'hgs'];
+;
+let mySize = 2;
+console.log(mySize);
+function OptParams(num1, num2) {
+    if (num2)
+        return num1 + num2;
+    return num1;
+}
+console.log(OptParams(10, 5));
+console.log(OptParams(100));
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,27 @@
-console.log('hello ');
+console.log('hello ');    
 
-// let age: number = 20;
+let age: number = 20;
+
+let bigNo = 10_00_222;
+console.log(bigNo);            //1000222
+
+let str = 'hjgskj';
+
+let tuple: [number, string] = [1, 'hgs'];
+
+//PascalCase enum & const generated more efficient code
+const enum Size {Small = 1, Medium, Large};
+
+let mySize: Size = Size.Medium;
+console.log(mySize);   //2
+
+//Functions with optional params and more strict validation
+function OptParams(num1: number, num2?: number) :number {
+  if(num2)
+    return num1 + num2;
+  return num1;
+}
+
+console.log(OptParams(10,5));      //15
+console.log(OptParams(100));      //100 
+// OptParams('jskfh',50);    This will throw error.
